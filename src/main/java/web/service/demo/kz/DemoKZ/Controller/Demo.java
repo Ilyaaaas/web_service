d469ee20ce2a12dee2dceb7f3095218bca25de19
Ilyas Akhmetov <axmetovia@gmail.com>
2018-04-03 16:24:44 +0600

    first commit

--- src/main/java/web/service/demo/kz/DemoKZ/Controller/Demo.java
+++ src/main/java/web/service/demo/kz/DemoKZ/Controller/Demo.java
@@ -10,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import web.service.demo.kz.DemoKZ.Model.UserModel;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/demo")
 public class Demo
 {
+
+    private static final String URL  = "jdbc:mysql://78.40.108.24:3306/web_service_db";
+    private static final String USER_NAME  = "root";
+    private static final String PASSWORD  = "Astana2018";
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> hello()
     {
@@ -31,10 +37,28 @@ public class Demo
     @RequestMapping(value = "/nek/array" , method = RequestMethod.GET)
     public ResponseEntity<ArrayList<UserModel>> array()
     {
+        System.out.println("get request is running");
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            Statement statement =  connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select customer_id, first_name, last_name from TEST");
+            while(resultSet.next())
+            {
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+                System.out.println("</br>");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("sql is done");
         ArrayList<UserModel> userModelArrayList = new ArrayList<UserModel>();
         userModelArrayList.add(new UserModel("Neka", "Nek"));
         userModelArrayList.add(new UserModel("Test", "TestValue"));
-        userModelArrayList.add(new UserModel("Test2", "TestValue23"));
+        userModelArrayList.add(new UserModel("Test777", "TestValue123"));
         return new ResponseEntity<ArrayList<UserModel>>(userModelArrayList, HttpStatus.CREATED);
     }
 }
